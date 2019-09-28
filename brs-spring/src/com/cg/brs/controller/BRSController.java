a5016681e14cad78c1a287339d7cde628f093e0b
Bandaru <OSIS11@192.168.1.6>
2019-09-28 17:06:00 +0530

    brs controller

--- brs-spring/src/com/cg/brs/controller/BRSController.java
+++ brs-spring/src/com/cg/brs/controller/BRSController.java
@@ -2,9 +2,11 @@ package com.cg.brs.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.cg.brs.dto.Booking;
 import com.cg.brs.service.BRSService;
 
 @Controller
@@ -13,9 +15,40 @@ public class BRSController {
 	@Autowired
 	BRSService brsService;
 
-	@RequestMapping(value = "/myfirst", method = RequestMethod.GET)
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String showHomePage() {
+		
 		return "jsp/home";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String showLoginPage() {
+		return "jsp/login";
+	}
+	
+	
+	@RequestMapping(value = "/aboutus",method = RequestMethod.GET)
+	public String showAboutUsPage() {
+		return "jsp/aboutUs";
+	}
+	
+	@RequestMapping(value = "/register",method = RequestMethod.GET)
+	public String showRegisterPage() {
+		return "jsp/register";
+	}
+	
+	@RequestMapping(value = "/help",method = RequestMethod.GET)
+	public String showHelpPage() {
+		return "jsp/help";
+	}
+	
+	@RequestMapping(value = "/test",method = RequestMethod.GET)
+	public String showTestPage(@ModelAttribute("test") Booking booking) {
+		return "jsp/test";
+	}
+	
+	public String showBusDetails() {
+		return "jsp/test";
 	}
 
 }
