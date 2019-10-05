195eb99c3b6a7357578e401e1a9590a671aafeaa
tejaswinibandaru <bandaruteju@gmail.com>
2019-10-05 10:07:08 +0530

    BRS Spring Boot MVC with Spring Data created

---
+++ BRSSpringBootMVC/src/main/java/com/cg/brsspringboot/BrsSpringBootMvcApplication.java
@@ -0,0 +1,14 @@
+package com.cg.brsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrsSpringBootMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrsSpringBootMvcApplication.class, args);
+		System.out.println("Hello Spring Boot");
+	}
+
+}
