b5d1e0e00871ef6112379b5fb37cf7face3db6ae
tejaswinibandaru <bandaruteju@gmail.com>
2019-10-05 10:22:25 +0530

    Updated Spring Boot MVC Project

--- BRSSpringBootMVC/src/main/java/com/cg/BrsSpringBootMVC/BrsSpringBootMvcApplication.java
+++ BRSSpringBootMVC/src/main/java/com/cg/BrsSpringBootMVC/BrsSpringBootMvcApplication.java
@@ -1,4 +1,4 @@
-package com.cg.brsspringboot;
+package com.cg.BrsSpringBootMVC;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
