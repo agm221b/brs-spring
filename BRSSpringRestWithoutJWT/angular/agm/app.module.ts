7288b34671a2cf9046bc426ebce507b908cadf25
agm221b <adityamishr123@gmail.com>
2019-10-21 09:41:48 -0800

    module and component

---
+++ BRSSpringRestWithoutJWT/angular/agm/app.module.ts
@@ -0,0 +1,41 @@
+﻿import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }  from './app.component';
+import { FormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
+
+
+import {RouterModule, Routes} from '@angular/router';
+
+import { AddBusComponent } from './app.addbuscomponent';
+import { ShowBusesComponent } from './app.showbusescomponent';
+import { AdminHomeComponent } from './app.adminhomecomponent';
+
+//{path: 'show/:text', component: ShowComponent},
+const myroutes:Routes= [
+    {path: '', redirectTo:'adminhome', pathMatch: 'full'},
+    {path: 'adminhome', component: AdminHomeComponent},
+    {path: 'addbus', component: AddBusComponent},
+    {path: 'showbuses', component: ShowBusesComponent},
+    
+];
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        RouterModule.forRoot(myroutes)
+    ],
+    declarations: [
+        AppComponent,
+        AddBusComponent,
+        ShowBusesComponent,
+        AdminHomeComponent
+		],
+    providers: [ ],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
